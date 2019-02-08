2604c3ee4fa3e8a83f2afbec85518ecfef08e4e0
nmerk <nadyamerk@gmail.com>
2019-02-08 18:25:19 +0300

    HW done

--- source/max.js
+++ source/max.js
@@ -1,3 +1,27 @@
 'use strict';
 
 const max = numbers => Math.max(...numbers);
+
+function gcd(a, b) {
+    while (a != 0 && b != 0) {
+        (a > b) ? a %= b : b %= a;
+    }
+    return a + b;
+}
+
+const euclid = function(...args) {
+    if (typeof(args[0]) == "undefined") {
+        return null;
+    }
+
+    if (typeof(args[1]) == "undefined") {
+        return args[0];
+    }
+
+    let result = gcd(args[0], args[1]);
+    for (let i = 2; i < args.length; ++i) {
+        result = gcd(result, args[i]);
+    }
+
+    return result;
+}
--- tests/euclid.js
+++ tests/euclid.js
@@ -28,4 +28,13 @@ QUnit.module('Тестируем функцию euclid', function () {
 		const temp = [ 80325, 55275, 8746650, 3000000, 45672375, 225, 54675 ];
 		assert.strictEqual(euclid(...[ ...temp, ...temp, ...temp, ...temp, ...temp ]), euclid(...temp));
 	});
+
+	QUnit.test('Мои тесты', function (assert) {
+		assert.strictEqual(euclid(8, 4, 2), 2, 'euclid(8, 4, 2) === 2');
+		assert.strictEqual(euclid(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+			89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199),
+			1, 'euclid(ряд простых чисел до 199 тут) === 1');
+		assert.strictEqual(euclid(11, 121, 1331, 14641, 161051), 11, 'euclid(степени 11 до пятой тут) === 11');
+		assert.strictEqual(euclid(3, 4, 8, 16, 32), 1, 'euclid(3, 4, 8, 16, 32) === 1');
+	});
 });
