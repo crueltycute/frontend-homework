961a2746789b1ee2441b256672971121f4926255
nmerk <nadyamerk@gmail.com>
2019-02-09 01:30:33 +0300

    add JSdocs, ===, !==, =>, foreach

--- source/max.js
+++ source/max.js
@@ -2,26 +2,35 @@
 
 const max = numbers => Math.max(...numbers);
 
-function gcd(a, b) {
-    while (a != 0 && b != 0) {
-        (a > b) ? a %= b : b %= a;
+/**
+ * Calculates the gcd of two numbers
+ *
+ * @param {number} first - The first number
+ * @param {number} second - The second number
+ * @returns {number}
+ */
+let gcd = (first, second) => {
+    while (first !== 0 && second !== 0) {
+        (first > second) ? first %= second : second %= first;
     }
-    return a + b;
-}
+    return first + second;
+};
 
-const euclid = function(...args) {
-    if (typeof(args[0]) == "undefined") {
+const euclid = (...args) => {
+    if (typeof args[0] === "undefined") {
         return null;
     }
 
-    if (typeof(args[1]) == "undefined") {
+    if (typeof args[1] === "undefined") {
         return args[0];
     }
 
     let result = gcd(args[0], args[1]);
-    for (let i = 2; i < args.length; ++i) {
-        result = gcd(result, args[i]);
-    }
+
+    [...args].splice(1, 2);
+    [...args].forEach(element => {
+        result = gcd(result, element);
+    });
 
     return result;
-}
+};
